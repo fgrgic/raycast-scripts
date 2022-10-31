a63921f96f527484450c4f3ebb4e6bc4d1dc21cf
Fran Grgić <grgicfran@gmail.com>
2022-10-31 15:49:17 +0100

    Easter output change to compact

--- easter-calculator.js
+++ easter-calculator.js
@@ -3,7 +3,7 @@
 // Required parameters:
 // @raycast.schemaVersion 1
 // @raycast.title Easter Calculator
-// @raycast.mode fullOutput
+// @raycast.mode compact
 
 // Optional parameters:
 // @raycast.icon 🐰
